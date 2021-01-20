1d41042f52dbcc895e6140b866859ac957c0f356
Your Name <you@example.com>
2021-01-20 14:39:11 -0600

    4 - need to fix api call?

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,18 +1,21 @@
 import axios from 'axios';
 
-export const FETCH_JOKE_START = "FETCH_QUOTE_START";
-export const FETCH_JOKE_FAIL = "FETCH_QUOTE_FAIL";
-export const FETCH_JOKE_SUCCESS = "FETCH_QUOTE_SUCCESS";
+export const FETCH_JOKE_START = "FETCH_JOKE_START";
+export const FETCH_JOKE_FAIL = "FETCH_JOKE_FAIL";
+export const FETCH_JOKE_SUCCESS = "FETCH_JOKE_SUCCESS";
 
 export const getJoke = () => dispatch => {
     dispatch({ type:FETCH_JOKE_START});
     axios 
         .get('GET https://icanhazdadjoke.com/')
+        
         .then((res)=>{
+            console.log('the get*****', res);
             dispatch({ type:FETCH_JOKE_SUCCESS, payload: res.data.joke});
         })
         .catch(err => {
             dispatch({ type:FETCH_JOKE_FAIL, payload: err.response.code});
         });
+        
 }
 
